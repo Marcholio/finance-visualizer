fa10bc1d24527eee8fdab5f8fbabce3340073ca2
markus <marcholio09@gmail.com>
2019-07-14 12:57:24 +0300

    Updates

---
+++ precache-manifest.a9909a5a265a2f1167281166c0d9299a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ce4516b9fa3b98bbf256929ee69ecf",
+    "url": "/finance-visualizer/index.html"
+  },
+  {
+    "revision": "1553a55251db7a7917de",
+    "url": "/finance-visualizer/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1d1392dd035be76acbff",
+    "url": "/finance-visualizer/static/js/2.096ca4c6.chunk.js"
+  },
+  {
+    "revision": "1553a55251db7a7917de",
+    "url": "/finance-visualizer/static/js/main.9f78b6ae.chunk.js"
+  },
+  {
+    "revision": "afb111882bdca82756f5",
+    "url": "/finance-visualizer/static/js/runtime~main.ed169ebd.js"
+  }
+]);
